8f91e7e8bc9f4990b809ee42e01452f1e33043c4
lbbetts <113577875+lbbetts@users.noreply.github.com>
2023-02-24 21:40:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 # funcs
 def get_fruityvice_data(fruit):
